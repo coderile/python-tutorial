6c75df7026dbe417d135da3fe6950aefda76567c
Amit Kumar <you@example.com>
2019-05-13 16:51:54 +0530

    Data Structure

---
+++ Data Structure/create_list.py
@@ -0,0 +1,22 @@
+# some of the built in data structure like list,tuples and dictionories
+
+
+# list has any kind of data type like string,boolean,number and list also
+
+friend = ['Amit ', "vikash", "rashmi"]
+matrix = [[0, 1], [2, 3]]
+
+print(matrix)
+print(friend)
+
+# concat
+print([0]*5 + friend)
+till_20 = list(range(1, 20))
+print(till_20)
+
+print('The chars of ', list('Hello world'))
+
+
+# check the length
+
+print(len(till_20))
