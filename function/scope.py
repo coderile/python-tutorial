b40ff14159b166812cbd73dee69e41416c13baf0
Amit Kumar <you@example.com>
2019-05-13 16:57:10 +0530

    Modifying the scope content

--- function/scope.py
+++ function/scope.py
@@ -12,3 +12,5 @@ print(message)
 
 # local variable
 # global variable
+# it is not good way to modify global variable  or avoid to use global variable
+# always try to use parameter or local variable
